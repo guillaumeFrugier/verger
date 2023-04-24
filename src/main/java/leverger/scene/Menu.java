69c71e63b5e057f9ab655773878fa67087040ef7
BuildTools <unconfigured@null.spigotmc.org>
2023-04-24 08:21:19 +0200

    changement sur le menu et ajout (potentiel à voir plus tard) des images lie au plateau (faut que je mette le corbeau aussi)

--- src/main/java/leverger/scene/Menu.java
+++ src/main/java/leverger/scene/Menu.java
@@ -10,24 +10,31 @@ import java.io.FileNotFoundException;
 public class Menu {
     //widget
     private BorderPane fond;
+    private VBox boutonOrganisation;
     private Scene scene;
     private Button boutonDebut;
+    private Button boutonQuitter;
     //gestion des images
     private ImageMenu imageMenu;
     //arriere plan du menu
     private BackgroundImage fondImage;
     private Background arrierePlan;
     //arriere plan bouton
-    private BackgroundImage fondBouton;
-    private Background boutonImage;
+    private BackgroundImage fondBoutonDebut;
+
+    private Background arrierPlanBoutonDebut;
+
+
 
 
 
     public Menu() throws FileNotFoundException {
         //widget
         this.fond = new BorderPane();
+        this.boutonOrganisation = new VBox();
         this.scene = new Scene(this.fond,800,800);
         this.boutonDebut = new Button("debut");
+        this.boutonQuitter = new Button("quitter");
         //gestion des images
         this.imageMenu = new ImageMenu();
 
@@ -35,17 +42,20 @@ public class Menu {
         this.fondImage = new BackgroundImage(imageMenu.getArrierePlanMenu(), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,BackgroundSize.DEFAULT);
         this.arrierePlan = new Background(this.fondImage);
         //arriere plan bouton
-        this.fondBouton = new BackgroundImage(imageMenu.getBoutonDebut(), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,BackgroundSize.DEFAULT);
-        this.boutonImage = new Background(this.fondBouton);
+        this.fondBoutonDebut = new BackgroundImage(imageMenu.getBoutonDebut(), BackgroundRepeat.NO_REPEAT,BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER,BackgroundSize.DEFAULT);
+        this.arrierPlanBoutonDebut = new Background(this.fondBoutonDebut);
 
     }
 
     public Scene initialisation(){
-        this.boutonDebut.setBackground(this.boutonImage);
+        this.boutonDebut.setBackground(this.arrierPlanBoutonDebut);
         this.boutonDebut.setMinHeight(100);
         this.boutonDebut.setMinWidth(500);
+        this.boutonQuitter.setMinWidth(500);
+        this.boutonQuitter.setMinHeight(100);
         this.fond.setBackground(this.arrierePlan);
         this.fond.setCenter(this.boutonDebut);
+        //this.boutonOrganisation.getChildren().addAll(this.boutonDebut,this.boutonQuitter);
         return scene;
     };
 }
