13daad182f0d48cf4aef825b9ad0afcb019af55f
Frugier guillaume <guillaume.frugier@etu.unilim.fr>
2023-04-27 10:08:19 +0200

    ajout de la scene plateau

---
+++ src/main/java/leverger/scene/Plateau.java
@@ -0,0 +1,16 @@
+package leverger.scene;
+
+import javafx.scene.layout.Pane;
+
+import javax.swing.text.html.ImageView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Plateau {
+    private Pane fond;
+
+    private List<ImageView> arbre;
+
+
+
+}
